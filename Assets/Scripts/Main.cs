850eee7fca1857bc6580750d1f090cacec477dce
HoGMaX <shogmax.git@gmail.com>
2020-04-24 01:08:10 +0300

    first commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Ui;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+   public Quations_list[] array_quation;
+   public Text Quations_text;
+   
+   public void OnClickPlay()
+   {
+        Quations_text.text = array_quation[Random.Range(0,array_quation.Length)].quation;
+   }
+}
+[System.Serializable]
+public class Quations_list 
+{
+    public string quation;
+    public string[] answers = new string [3];
+}
