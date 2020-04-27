0ffefc24807d356dd2278d4165b7b651d08f630f
HoGMaX <shogmax.git@gmail.com>
2020-04-27 14:08:09 +0300

    master

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,11 +6,28 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
    public Quations_list[] array_quation;
-   public Text Quations_text;
+   public Text quations_text;
+   public List<object> quation_list;
+   public Text[] answers_text;
+   int rand_quattion;
    
    public void OnClickPlay()
    {
-        Quations_text.text = array_quation[Random.Range(0,array_quation.Length)].quation;
+     quation_list = new List<object>(array_quation);
+     Quation_Generate();
+   }   
+   void Quation_Generate(){
+        rand_quattion = Random.Range(0,quation_list.Count);
+        Quations_list current_quation = quation_list[rand_quattion] as Quations_list;
+        quations_text.text = current_quation.quation;
+        quations_text.text = array_quation[Random.Range(0,array_quation.Length)].quation;
+        for(int i = 0; i < current_quation.answers.Length; i++){
+          answers_text[i].text = current_quation.answers[i]; 
+        }
+   } 
+   public void Answers_button(){
+       quation_list.RemoveAt(rand_quattion);
+       Quation_Generate();
    }
 }
 [System.Serializable]
